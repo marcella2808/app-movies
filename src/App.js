2b9949c5ef13c13dcecb8fb2ae5546667315ea6a
marcella2808 <marcella.carneiro@a.fecaf.com.br>
2024-11-07 18:25:57 -0300

    Criando App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Container from "./components/Container";
+import Card from "./components/Card";
+import videos from "./json/db.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header />
+            <Banner img="home" />
+            <Container>
+                <h2>Geografia</h2>
+                <section className="cards">
+                    { videos.map((video) => <Card id={video.id} key={video.id} />) }
+                </section>
+            </Container>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
